cc12d619cfe6ff36bbb8e3287d2cc35fb6280c46
ashdephoenix123 <akashsarki12345@gmail.com>
2026-06-15 21:01:26 +0530

    starter files

---
+++ index.js
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import OpenAI from "openai";
+
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const models = await client.models.list();
+for (const i of models.data) {
+  console.log(i.id);
+}
+
+// const response = await client.responses.create({
+//   model: "gpt-4.1-mini-2025-04-14",
+//   input: "Write a one-sentence bedtime story about a unicorn.",
+// });
+
+// console.log(response);
