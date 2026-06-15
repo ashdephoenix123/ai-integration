0a937ec153cc742c7ce55cbd1c54cdb03a1920c7
Akash Sarki <102145279+ashdephoenix123@users.noreply.github.com>
2026-06-15 15:51:00 +0000

    added typein package json

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const models = await client.models.list();
-for (const i of models.data) {
-  console.log(i.id);
-}
+// const models = await client.models.list();
+// for (const i of models.data) {
+//   console.log(i.id);
+// }
 
-// const response = await client.responses.create({
-//   model: "gpt-4.1-mini-2025-04-14",
-//   input: "Write a one-sentence bedtime story about a unicorn.",
-// });
+const response = await client.responses.create({
+  model: "gpt-4.1-mini-2025-04-14",
+  input: "Write a one-sentence bedtime story about a unicorn.",
+});
 
-// console.log(response);
+console.log(response.output_text);
